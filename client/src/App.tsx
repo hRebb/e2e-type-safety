c73f8c647032cd7dd9574326bf64e131d9d13539
hRebb <hamza.rebb@gmail.com>
2024-01-01 15:45:21 +0100

    create first const using user object

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,17 @@ import { User } from "./types"
 
 function App() 
 {
+
+  const users: User[] = [
+    {
+      name: 'Prisma Fan',
+      message: [
+      { body: 'Prisma rocks!!' },
+      { body: 'Did I mention I love Prisma?' }
+      ]
+    },
+  ]
+
   return (
     <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
       <h2 className="text-4xl text-yellow-500">
